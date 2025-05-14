cfaed522c8315284932a6e03c377115f91469825
RobinRees <Reehees12@gmail.com>
2025-05-14 12:37:46 +0200

    Small change

--- server.js
+++ server.js
@@ -2,9 +2,18 @@
 import { createOptions } from "./utilities.js";
 
 async function handler(request) {
+    const url = new URL(request.url);
+
 
     if (request.method === "OPTIONS") {
         return new Response(null, createOptions())
+    }
+
+    
+    if (request.method === "GET" && url.pathname === "") {
+
+
+        
     }
 
 
