f434ccce056f05e59c5232135d3f262379093a91
Justin Siebert <19justinsiebert99@gmail.com>
2025-05-14 15:12:56 +0200

    Squashed commit of the following:
    commit 19b94bdb1a259b24cc94740275f686bcbe696a64
    Author: Justin Siebert <19justinsiebert99@gmail.com>
    Date:   Wed May 14 15:10:11 2025 +0200
    
        test for merge
    
    commit fab3a9ac569d41056810a733a0c618beafbc24e0
    Author: Justin Siebert <19justinsiebert99@gmail.com>
    Date:   Wed May 14 15:07:47 2025 +0200
    
        created class and function user
    
        created class User that creats instance of user and function starting for saving user in json file. Not finished

--- utilities.js
+++ utilities.js
@@ -12,3 +12,60 @@ export function createOptions(status = 200) {
 export function checkContentType(contentType) {
     return contentType === "application/json"
 }
+
+
+
+export class User {
+    constructor (data = {}){
+        this._username = data.username
+        this._score = data.score
+    }
+
+    get username(){
+        return this._username
+    }
+
+    set username(value){
+        if (typeof value !== "string"){
+            return console.log("ERROR")
+        }
+
+        this._username = value
+    }
+
+    get score(){
+        return this._score
+    }
+
+    set score(value){
+        if (typeof value !== "number"){
+            return console.log("ERROR")
+        }
+
+       this._score = value
+    }
+
+    
+}
+
+export async function createUser(username, score){
+    const user = new User({ username: `${username}`, score: `${score}` })
+    const response = await fetch("fil till alla användare")
+    let users = await response.json()
+    
+    const existingUser = users.find(x => x.username === user.username)
+
+    if(existingUser){
+        // här händer något med score om användare redan finns. Bör lägga in att det endast uppdatera 
+        // score är högre
+    } else {
+        // här addar vi user i listan med score
+    }
+}
+// const user = new User({ username: "Anna", score: 10 });
+
+let myOPT = {
+    method: "POST",
+    headres: {"Content-Type": "application/json"},
+    body: JSON.stringify({"username": username})
+}
