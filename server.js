5f4deb1b414a9271afc56b3ea4e19927d93254f8
RobinRees <Reehees12@gmail.com>
2025-05-14 15:13:08 +0200

    Test

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ async function handler(request) {
     const url = new URL(request.url);
     const contentType = request.headers.get("Content-Type")
 
+    
+
     if (request.method === "OPTIONS") {
         return new Response(null, createOptions())
     }
 
     if (request.method === "GET" && url.pathname === "/carbonaraGame") {
         if (checkContentType(contentType)) {
-            const fetchedRecipe = Deno.readTextFileSync("test.json");
+            console.log("Sad")
+            const fetchedRecipe = Deno.readTextFileSync("database/test.json");
             console.log(fetchedRecipe);
 
             return new Response(fetchedRecipe, createOptions());
--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
 const main = document.querySelector("main");
 
 function test1() {
-    return fetch("http://localhost:8000/carbonaraGame")
+    return fetch("http://localhost:8000/carbonaraGame", {
+        method: "GET", 
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
         .then(response => {
             const createDivOne = document.createElement("div");
             createDivOne.textContent = `Status: ${response.status}`;
@@ -20,4 +25,33 @@ function test1() {
         });
 }
 
+
+
+function test2() {
+    return fetch("http://localhost:8000/carbonaraGame", {
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body : JSON.stringify( { name: "mordor" } )
+    })
+        .then(response => {
+        const createDivOne = document.createElement("div");
+        createDivOne.textContent = `Status: ${response.status}`;
+        main.appendChild(createDivOne);
+        return response.json();
+        })
+        .then(data => {
+        const createDivTwo = document.createElement("div");
+        createDivTwo.textContent = `Data: ${JSON.stringify(data)}`;
+        main.appendChild(createDivTwo);
+        })
+        .catch(error => {
+        const errorDiv = document.createElement("div");
+        errorDiv.textContent = `Fel: ${error.message}`;
+        main.appendChild(errorDiv);
+        });
+}
+
 test1()
+test2()
