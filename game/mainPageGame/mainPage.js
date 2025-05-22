77daab891dab7d5117acc7c078a3e65c1ea242c0
RobinRees <Reehees12@gmail.com>
2025-05-22 16:15:53 +0200

    Test

--- game/mainPageGame/mainPage.js
+++ game/mainPageGame/mainPage.js
@@ -117,12 +117,9 @@ async function createChoices() {
                 lives--
                 livesBox.innerHTML = `Life left: ${lives}`;
                 if (lives === 0) {
-                    await fetch("http://localhost:8000/updateScore", {
-                        method: "PATCH",
-                        headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify({ score: currentScore })
-                    });
-                    window.location.href = "../gameOver"
+
+                    createTriviaQuestion()
+
                 }
             }
         });
@@ -177,5 +174,94 @@ logOutButton.addEventListener("click", async () => {
 })
 
 
+
+
+
+function decodeHTML(html) {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
+
+
+async function createTriviaQuestion() {
+
+    const multipleAnswersBox = document.getElementById("multipleAnswersBox");
+    multipleAnswersBox.innerHTML = "";
+    const questionBox = document.getElementById("questionBox")
+    questionBox.style.display = "flex";
+    const response = await fetch("https://opentdb.com/api.php?amount=1");
+    const data = await response.json();
+
+    const questionData = data.results[0];
+    const question = questionData.question;
+    const correctAnswer = questionData.correct_answer;
+    const incorrectAnswers = questionData.incorrect_answers;
+
+
+    const allAnswers = [
+        {
+            answer: correctAnswer,
+            isCorrect: true
+        },
+        ...incorrectAnswers.map(answer => ({
+            answer: answer,
+            isCorrect: false
+        }))
+    ];
+
+    const shuffledAnswers = allAnswers.sort(() => Math.random() - 0.5);
+
+    const questionText = document.getElementById("questionText");
+    questionText.textContent = decodeHTML(question);
+
+    shuffledAnswers.forEach(choice => {
+        const multipleAnswersBox = document.getElementById("multipleAnswersBox")
+        const div = document.createElement("div");
+        div.classList.add("triviaChoice");
+
+        const text = document.createElement("p");
+        text.textContent = decodeHTML(choice.answer);
+        div.appendChild(text);
+        multipleAnswersBox.appendChild(div);
+
+        div.addEventListener("click", async () => {
+            div.classList.add("clicked");
+
+            if (choice.isCorrect) {
+                div.style.backgroundColor = "lightGreen";
+                const winText = document.getElementById("winText");
+                winText.style.display = "block";
+                lives++;
+                livesBox.innerHTML = `Life left: ${lives}`;
+                // Life + 1;
+
+                setTimeout(() => {
+                    questionBox.style.display = "none";
+                    winText.style.display = "none";
+                }, 2000)
+                
+            } else {
+                div.style.backgroundColor = "tomato";
+                const spanText = document.getElementById("correctAnswer");
+                spanText.textContent = `correct answer was: ${correctAnswer}`;
+                const loseText = document.getElementById("lostText");
+                loseText.style.display = "block";
+                await fetch("http://localhost:8000/updateScore", {
+                    method: "PATCH",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ score: currentScore })
+                });
+                    
+
+                setTimeout(() => {
+                    console.log("Kommer vi in här?")
+                    window.location.href = "/gameOver";
+                }, 2000)
+                // redirect - Loosescreen timer? 
+            }
+        })
+    })
+}
 
 // createChoices()
