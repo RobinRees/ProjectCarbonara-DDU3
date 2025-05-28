08c457c6d8b53a6d5d68683180c9e0d221184d62
Justin Siebert <19justinsiebert99@gmail.com>
2025-05-28 12:30:11 +0200

    added patch

--- game/utilities.js
+++ game/utilities.js
@@ -6,7 +6,7 @@ export function createOptions(status = 200) {
         status: status,
         headers: {
             "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, DELETE, OPTIONS",
+            "Access-Control-Allow-Methods": "GET, POST, DELETE, PATCH, OPTIONS",
             "Access-Control-Allow-Headers": "Content-Type",
         }
     };
